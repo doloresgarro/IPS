b4f47c627ca32deb27bb3d5a33fb9d31d136724d
Dolores <129607981+doloresgarro@users.noreply.github.com>
2023-12-13 20:43:31 -0300

    Laboratorio 1 corregido

--- IPS-pu1/Ejercicio 2/graficoSenialConFiltro.m
+++
@@ -1,8 +0,0 @@
-% Función que grafica la señal pasada por el filtro
-function graficoSenialConFiltro(h)   
-    n = 0:18000;
-    stem(n,h);
-    xlabel("n");
-    ylabel("h[n]");
-    title("Señal con filtro");
-end
